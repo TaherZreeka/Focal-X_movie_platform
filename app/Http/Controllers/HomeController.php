a377a4e3845580c521e7e1009a954e14bd762d25
Taher Zreek <taherazreek@gmail.com>
2025-08-05 11:17:02 +0300

    Update HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,11 +2,6 @@
 
 namespace App\Http\Controllers;
 
-<<<<<<< HEAD
-use App\Enums\UserRole;
-use Illuminate\Http\Request;
-=======
->>>>>>> origin/nour
 use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
@@ -17,22 +12,14 @@ class HomeController extends Controller
     }
 
   public function index()
-<<<<<<< HEAD
         {
-             if (Auth::check() && Auth::user()->role === UserRole::Admin->value)
-             return view('admin.dashboard');
-             else
-                return view('content_admin.dashboard');
-=======
-        { 
             $user = Auth::user();
              if ( $user->hasRole('admin')) {
                 return view('admin.dashboard');
             } elseif ($user->hasRole('content_admin')) {
                 return view('content_admin.dashboard');
             } else {
-                Auth::logout(); 
+                Auth::logout();
                 return redirect('/login')->withErrors(['role' => 'Unauthorized role.']);            }
->>>>>>> origin/nour
         }
 }
